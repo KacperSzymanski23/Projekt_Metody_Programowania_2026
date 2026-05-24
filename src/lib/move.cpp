b339aae6c9f8066b532608de53b2f326a6a064ce
KacperSzymanski23 <kacper5szymanski@gmail.com>
2026-05-24 17:05:13 +0200

    feat(Move): Dodano klasę Move do projektu

--- src/lib/move.cpp
+++ src/lib/move.cpp
@@ -1,5 +1,18 @@
 #include "move.h"
 
 namespace TicTacToe {
-		// TODO: Kod do zrobienia
+
+		Move::Move(size_t x, size_t y)
+			: m_x(x)
+			, m_y(y) {
+		}
+
+		size_t Move::x() const {
+				return m_x;
+		}
+
+		size_t Move::y() const {
+				return m_y;
+		}
+
 } // namespace TicTacToe
--- src/lib/move.h
+++ src/lib/move.h
@@ -1,8 +1,21 @@
 #ifndef MOVE_H
 #define MOVE_H
 
+// STD
+#include <cstddef>
+
 namespace TicTacToe {
-		// TODO: Kod do zrobienia
+		class Move {
+			  public:
+				Move(size_t x, size_t y);
+
+				[[nodiscard]] size_t x() const;
+				[[nodiscard]] size_t y() const;
+
+			  private:
+				size_t m_x;
+				size_t m_y;
+		};
 } // namespace TicTacToe
 
 #endif /* MOVE_H */
