d6ac91035e927ede1c84372388390f10d609fb32
KacperSzymanski23 <kacper5szymanski@gmail.com>
2026-05-24 18:01:07 +0200

    feat(Exceptions): Dodano klasy wyjątków do projektu

--- src/lib/myexceptions.h
+++ src/lib/myexceptions.h
@@ -1,6 +1,38 @@
 #ifndef MY_EXCEPTIONS_H
 #define MY_EXCEPTIONS_H
 
-namespace MyExceptions {} // namespace MyExceptions
+// STD
+#include <stdexcept>
+#include <string>
+
+namespace MyExceptions {
+		class InvalidArgumentException : public std::invalid_argument {
+			  public:
+				explicit InvalidArgumentException(const std::string &message)
+					: std::invalid_argument(message) {
+				}
+		};
+
+		class InvalidMoveException : public std::logic_error {
+			  public:
+				explicit InvalidMoveException(const std::string &message)
+					: std::logic_error(message) {
+				}
+		};
+
+		class InvalidSymbolException : public std::invalid_argument {
+			  public:
+				explicit InvalidSymbolException(const std::string &message)
+					: std::invalid_argument(message) {
+				}
+		};
+
+		class GameAlreadyOverException : public std::logic_error {
+			  public:
+				explicit GameAlreadyOverException(const std::string &message)
+					: std::logic_error(message) {
+				}
+		};
+} // namespace MyExceptions
 
 #endif /* MY_EXCEPTIONS_H */
