a30e1ff7f7de6a0dc27fc0f859820f171fde9cc1
KacperSzymanski23 <kacper5szymanski@gmail.com>
2026-05-24 17:51:19 +0200

    feat(Strategy): Dodano klasę Strategy do projektu

--- src/lib/strategy.h
+++ src/lib/strategy.h
@@ -1,8 +1,25 @@
 #ifndef STRATEGY_H
 #define STRATEGY_H
 
+#include "board.h"
+#include "move.h"
+
 namespace TicTacToe {
-		// TODO: Kod do zrobienia
+
+		class Strategy {
+			  public:
+				Strategy() = default;
+
+				Strategy(const Strategy &) = default;
+				Strategy &operator=(const Strategy &) = default;
+
+				Strategy(Strategy &&) = default;
+				Strategy &operator=(Strategy &&) = default;
+
+				virtual ~Strategy() = default;
+
+				virtual Move chooseMove(const Board &board, char symbol) = 0;
+		};
 } // namespace TicTacToe
 
 #endif /* STRATEGY_H */
