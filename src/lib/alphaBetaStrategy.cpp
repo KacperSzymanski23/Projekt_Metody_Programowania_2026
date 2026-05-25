f970f4afb8c13fe7590acf63fbda463f1c70e1b7
KacperSzymanski23 <kacper5szymanski@gmail.com>
2026-05-25 11:17:16 +0200

    feat(AlphaBeta): Dodano klasę dla strategii AlphaBeta do projektu

--- src/lib/alphaBetaStrategy.cpp
+++ src/lib/alphaBetaStrategy.cpp
@@ -1,5 +1,119 @@
 #include "alphaBetaStrategy.h"
+#include "myexceptions.h"
+#include "utils.h"
 
 namespace TicTacToe {
-		// TODO: Kod do zrobienia
+		Move AlphaBetaStrategy::chooseMove(Board<char> &board, char symbol) {
+				if (symbol != 'X' && symbol != 'O') {
+						throw MyExceptions::InvalidArgumentException("Symbol must be X or O");
+				}
+				if (board.full()) {
+						throw MyExceptions::InvalidArgumentException("Board is full");
+				}
+
+				int32_t bestScore{std::numeric_limits<int32_t>::min()};
+				size_t bestColumn{};
+				size_t bestRow{};
+
+				bool found{false};
+
+				constexpr int32_t ALPHA_INIT = std::numeric_limits<int32_t>::min();
+				constexpr int32_t BETA_INIT = std::numeric_limits<int32_t>::max();
+
+				const size_t BOARD_SIZE = board.size();
+
+				for (size_t row{}; row < BOARD_SIZE; ++row) {
+						for (size_t column{}; column < BOARD_SIZE; ++column) {
+								if (board(column, row) == '\0') {
+										board(column, row) = symbol;
+
+										const int32_t SCORE = alphaBeta(board, symbol, true, ALPHA_INIT, BETA_INIT);
+
+										board(column, row) = '\0';
+
+										if (SCORE > bestScore || !found) {
+												bestScore = SCORE;
+												bestColumn = column;
+												bestRow = row;
+												found = true;
+										}
+								}
+						}
+				}
+
+				if (!found) {
+						throw MyExceptions::InvalidArgumentException("No move found");
+				}
+
+				return {bestColumn, bestRow};
+		}
+
+		int32_t AlphaBetaStrategy::evaluate(const Board<char> &board, char forSymbol) {
+				if (Utils::hasWon(board, forSymbol)) {
+						return 1;
+				}
+
+				if (Utils::hasWon(board, Utils::getOpponent(forSymbol))) {
+						return -1;
+				}
+
+				return 0;
+		}
+
+		int32_t AlphaBetaStrategy::alphaBeta(Board<char> &board, char symbol, bool isMaximizing, int32_t alpha, int32_t beta) {
+				const int32_t SCORE = evaluate(board, symbol);
+
+				if (SCORE != 0 || board.full()) {
+						return SCORE;
+				}
+
+				const size_t BOARD_SIZE = board.size();
+				const char CURRENT_SYMBOL = isMaximizing ? symbol : Utils::getOpponent(symbol);
+
+				if (isMaximizing) {
+						int32_t bestScore{std::numeric_limits<int32_t>::min()};
+
+						for (size_t row{}; row < BOARD_SIZE; ++row) {
+								for (size_t column{}; column < BOARD_SIZE; ++column) {
+										if (board(column, row) == '\0') {
+												board(column, row) = CURRENT_SYMBOL;
+
+												const int32_t TEMP_SCORE = alphaBeta(board, CURRENT_SYMBOL, false, alpha, beta);
+
+												board(column, row) = '\0';
+
+												bestScore = std::max(bestScore, TEMP_SCORE);
+												alpha = std::max(alpha, bestScore);
+
+												if (beta <= alpha) {
+														return bestScore;
+												}
+										}
+								}
+						}
+				}
+
+				int32_t bestScore{std::numeric_limits<int32_t>::max()};
+
+				for (size_t row{}; row < BOARD_SIZE; ++row) {
+						for (size_t column{}; column < BOARD_SIZE; ++column) {
+								if (board(column, row) == '\0') {
+										board(column, row) = CURRENT_SYMBOL;
+
+										const int32_t TEMP_SCORE = alphaBeta(board, CURRENT_SYMBOL, true, alpha, beta);
+
+										board(column, row) = '\0';
+
+										bestScore = std::min(bestScore, TEMP_SCORE);
+										beta = std::min(beta, bestScore);
+
+										if (beta <= alpha) {
+												return bestScore;
+										}
+								}
+						}
+				}
+
+				return bestScore;
+		}
 } // namespace TicTacToe
--- src/lib/alphaBetaStrategy.h
+++ src/lib/alphaBetaStrategy.h
@@ -1,8 +1,19 @@
 #ifndef ALPHA_BETA_STRATEGY_H
 #define ALPHA_BETA_STRATEGY_H
 
+#include "strategy.h"
+
 namespace TicTacToe {
-		// TODO: Kod do zrobienia
+		class AlphaBetaStrategy : public Strategy {
+			  public:
+				AlphaBetaStrategy() = default;
+				~AlphaBetaStrategy() override = default;
+				Move chooseMove(Board<char> &board, char symbol) override;
+
+			  private:
+				int32_t evaluate(const Board<char> &board, char forSymbol);
+				int32_t alphaBeta(Board<char> &board, char symbol, bool isMaximizing, int32_t alpha, int32_t beta);
+		};
 } // namespace TicTacToe
 
 #endif /* ALPHA_BETA_STRATEGY_H */
